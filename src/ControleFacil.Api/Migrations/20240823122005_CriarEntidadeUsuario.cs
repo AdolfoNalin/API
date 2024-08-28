9a7a212e84957b21219765dc6013aacee7f12c45
AdolfoNalin <nalinadolfo33@gmail.com>
2024-08-28 12:04:19 -0300

    Criando class migrations

---
+++ src/ControleFacil.Api/Migrations/20240823122005_CriarEntidadeUsuario.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ControleFacil.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarEntidadeUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usuario",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "varchar", nullable: false),
+                    Password = table.Column<string>(type: "varchar", nullable: false),
+                    DataCadastro = table.Column<string>(type: "varchar", nullable: false),
+                    DataInativacao = table.Column<string>(type: "varchar", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuario", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usuario");
+        }
+    }
+}
