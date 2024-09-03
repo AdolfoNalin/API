9cf293f6a8d9cf47d4ade51cc01ae021656707ea
AdolfoNalin <nalinadolfo33@gmail.com>
2024-09-03 15:34:51 -0300

    Craindo a class NaturezaLancamentoMap, tables no banco de dados

---
+++ src/ControleFacil.Api/Data/Mappings/NaturezaLancamentoMap.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleFacil.Api.Damain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ControleFacil.Api.Data.Mappings
+{
+    public class NaturezaLancamentoMap : IEntityTypeConfiguration<NaturezaLancamento>
+    {
+        public void Configure(EntityTypeBuilder<NaturezaLancamento> builder)
+        {
+            builder.ToTable("natureza_lancamento").HasKey(p => p.ID);
+            builder.HasOne(u => u.Usuario).WithMany().HasForeignKey(fk => fk.IdUser);
+            builder.Property(p => p.Descricao).HasColumnType("varchar").IsRequired();
+            builder.Property(p => p.Obs).HasColumnType("varchar").IsRequired();
+            builder.Property(p => p.DataCadastro).HasColumnType("varchar");
+            builder.Property(p => p.DataInativacao).HasColumnType("varchar").IsRequired();
+        }
+    }
+}
