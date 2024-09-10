ae84a3a47e3c750c9e000532b2bdc86ddfce1dda
AdolfoNalin <nalinadolfo33@gmail.com>
2024-09-09 21:23:47 -0300

    Corrindo erro no método obter All passando o parametro lond idUser

--- src/ControleFacil.Api/Domain/Repository/Interface/IRepository.cs
+++ src/ControleFacil.Api/Domain/Repository/Interface/IRepository.cs
@@ -7,8 +7,8 @@ namespace ControleFacil.Api.Damain.Repository.Interface
 {
     public interface IRepository<T, I> where T : class
     {
-        Task<IEnumerable<T>> Obter();
-        Task<T?> Obter(I id);
+        Task<IEnumerable<T>> Obter(I idUser);
+        Task<T?> Obter(I id, I idUser);
         Task<T> Add(T entidade);
         Task<T> Update (T entidade);
         Task Delete(T entidade);
