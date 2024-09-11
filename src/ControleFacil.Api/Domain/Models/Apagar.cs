8ed6c515a3ed16866843e9dd8726f1b900a9a797
AdolfoNalin <nalinadolfo33@gmail.com>
2024-09-11 12:55:26 -0300

    Criando a class modelo do Apagar

---
+++ src/ControleFacil.Api/Domain/Models/Apagar.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleFacil.Api.Contract.Usuario;
+
+namespace ControleFacil.Api.Damain.Models
+{
+    public class Apagar
+    {
+        [Key]
+        public long ID { get; set; }
+        [Required]
+        public long IdUser { get; set; }
+        public Usuario Usuario { get; set; }
+        [Required]
+        public long IdNL { get; set; }
+        public NaturezaLancamento NaturezaLancamento { get; set; }
+        [Required(ErrorMessage = "Campo de Descrição é obrigatório!")]
+        public string Descricao { get; set; } = String.Empty;
+        public string? Obs {get; set;} = String.Empty;
+        [Required(ErrorMessage = "Valor original é obrigatório")]
+        public float ValorOriginal { get; set; }
+        [Required(ErrorMessage = "Valor Pago é obrigatório")]
+        public float ValorPago { get; set; }
+        [Required]
+        public string DataCadastro { get; set; }
+        [Required(ErrorMessage = "Data de vencimento é obrigatório")]
+        public string? DataVencimento { get; set; }
+        public string? DataInativacao { get; set; }
+        public string? DataRefencia { get; set; }
+        public string? DataPagamento { get; set; }
+    }
+}
