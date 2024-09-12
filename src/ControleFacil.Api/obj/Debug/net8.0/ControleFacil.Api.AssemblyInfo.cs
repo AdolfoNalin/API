a80cc826016fa9a74af4ec4b82bb5e5683f61d61
AdolfoNalin <nalinadolfo33@gmail.com>
2024-09-11 21:01:11 -0300

    Arquivos de execução

--- src/ControleFacil.Api/obj/Debug/net8.0/ControleFacil.Api.AssemblyInfo.cs
+++ src/ControleFacil.Api/obj/Debug/net8.0/ControleFacil.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleFacil.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46f3d3f57644b0206510cd910ef19d988a66731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4bb3c317ff03d085f4162259db767b56c2fd4aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleFacil.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleFacil.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
