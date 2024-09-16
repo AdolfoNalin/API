b42a113d87769ea20f813281a514822b684ce0eb
AdolfoNalin <nalinadolfo33@gmail.com>
2024-09-16 13:47:22 -0300

    Criando class AreceberProfile

---
+++ src/ControleFacil.Api/AutoMapper/AreceberProfile.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ControleFacil.Api.Contract.Apagar;
+using ControleFacil.Api.Damain.Models;
+
+namespace ControleFacil.Api.AutoMapper
+{
+    public class AreceberProfile : Profile
+    {
+        public AreceberProfile()
+        {
+            CreateMap<Areceber, AreceberRequestContract>().ReverseMap();
+            CreateMap<Areceber, AreceberResponseContract>().ReverseMap();
+        }
+    }
+}
