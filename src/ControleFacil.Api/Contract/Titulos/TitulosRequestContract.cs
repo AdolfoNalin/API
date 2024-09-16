2bcb124ee7bb0d6b45c0fb971b14d821f7a53635
AdolfoNalin <nalinadolfo33@gmail.com>
2024-09-16 13:55:49 -0300

    Criando TitulosRequestContract class abstrata

--- src/ControleFacil.Api/Contract/Titulos/TitulosRequestContract.cs
+++ src/ControleFacil.Api/Contract/Titulos/TitulosRequestContract.cs
@@ -7,6 +7,12 @@ namespace ControleFacil.Api.Contract.Titulos
 {
     public abstract class TitulosRequestContract
     {
+         public long IdNL { get; set; }
+        public string Descricao { get; set; } = String.Empty;
+        public string Obs { get; set; } = String.Empty;
+        public float ValorOriginal { get; set; }
+        public string? DataRefencia {get; set;}
+        public string DataVencimento { get; set; } = String.Empty;
         
     }
 }
