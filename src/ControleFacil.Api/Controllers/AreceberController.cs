73ce843a630143f32c1a23bd9d5180005ea1aeb5
AdolfoNalin <nalinadolfo33@gmail.com>
2024-09-17 13:08:39 -0300

    Criando a classe AreceberController

---
+++ src/ControleFacil.Api/Controllers/AreceberController.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleFacil.Api.Contract.Areceber;
+using ControleFacil.Api.Damain.Services.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleFacil.Api.Controllers
+{
+    [ApiController]
+    [Route("titulosareceber")]
+    public class AreceberController : BaseController
+    {
+        private readonly IService<AreceberRequestContract, AreceberResponseContract, long> _areceber;
+        private long _idUser;
+
+        public AreceberController(IService<AreceberRequestContract, AreceberResponseContract, long> areceber)
+        {
+            _areceber = areceber;
+        }
+
+        #region  Insert
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Insert(AreceberRequestContract entidade)
+        {
+            try
+            {
+                _idUser = GetUserLoginId();
+                return Created("", await _areceber.Insert(entidade: entidade, idUser: _idUser));
+            }
+            catch(Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        #endregion
+    
+        #region Update
+        [HttpPut]
+        [Route("{id}")]
+        [Authorize]
+        public async Task<IActionResult> Update(long id, AreceberRequestContract contract)
+        {
+            try
+            {
+                _idUser = GetUserLoginId();
+                return Ok(await _areceber.Update(id:id,entidade: contract, idUser: _idUser));
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        #endregion
+    
+        #region Delete
+        [HttpDelete]
+        [Route("{id}")]
+        [Authorize]
+        public async Task<IActionResult> Delete(long id)
+        {
+            try
+            {
+                _idUser = GetUserLoginId();
+                await _areceber.Inativar(id: id, idUser: _idUser);
+                return NoContent();
+            }
+            catch (System.Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        #endregion
+    
+        #region Obter All
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Obter()
+        {
+            try
+            {
+                _idUser = GetUserLoginId();
+                return Ok(await _areceber.Obter(idUser: _idUser));
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        #endregion
+    
+        #region Obter Id
+        [HttpGet]
+        [Route("{id}")]
+        [Authorize]
+        public async Task<IActionResult> Obter(long id)
+        {
+            try
+            {
+                _idUser = GetUserLoginId();
+                return Ok(await _areceber.Obter(id: id, idUser: _idUser));
+            }
+            catch (System.Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
